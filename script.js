6a055acafb4c055636cdc3c924328800ce38c947
mariam <mariamabdelhaq883@gmail.com>
2025-06-28 18:48:22 +0300

    edit nav

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ btn.forEach(button =>{
     });
 });
 
-const video =document.getElementById=("video");
+const video =document.getElementById("video");
 window.addEventListener("scroll", () =>{
     const rect = video.getBoundingClientRect().top;
         if (rect < window.innerHeight - 100){
@@ -51,3 +51,19 @@ window.addEventListener("scroll", () =>{
             video.pause();
         }  
     });
+
+const menu = document.getElementById("mobile-menu");
+const menubtn= document.getElementById("menu-icon");
+const menua =document.querySelectorAll(".mobile-menu a");
+const animation ="animation-menu";
+menubtn.addEventListener("click", () => {
+ if (menu.style.display === "flex") {
+    menu.style.display = "none";
+    menua.forEach(link =>{link.style.display="none"});
+  } 
+  else {
+    menu.style.display = "flex";
+    menua.forEach(link =>{link.style.display="flex"});
+    menu.classList.add(animation);
+  }
+})    
